1321c426c2cdb9392eade16a04f54fbe59e321b2
dopeldev <322kuroneko@gmail.com>
2024-03-23 23:22:03 -0500

    restructured backend URLs

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.http import HttpResponse
 # import drf_yasg views
 from drf_yasg.views import get_schema_view
@@ -45,8 +45,13 @@ schema_view = get_schema_view(
 )
 
 # Swagger UI URL
-urlpatterns += [ 
+urlpatterns_swagger = [ 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] 
+
+# API URL
+urlpatterns += [
+    path('api/v1/', include(urlpatterns_swagger) ),
+]
