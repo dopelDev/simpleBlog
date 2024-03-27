a781de674ef0c4aeb5cfea5a9701d873d5904deb
dopeldev <322kuroneko@gmail.com>
2024-03-27 05:21:48 -0500

    add username in myUser model

--- backend/appBlog/models.py
+++ backend/appBlog/models.py
@@ -6,22 +6,26 @@ from django.utils import timezone
 # further extending the AbstractBaseUser and PermissionsMixin classes and avoid colisions with the default User model
 
 class myUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('El email es obligatorio')
+            raise ValueError('The email is required')
+        if not username:
+            raise ValueError('The username is required')
+        
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
 
 class myUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
